15bd1ca7438feb9bad4c8cf0888f3d1ab5f01f3d
李奕逊 <liyixun@bytedance.com>
2021-12-12 22:54:55 +0800

    feat: test coverage

--- src/hooks/__tests__/useCheckbox.test.ts
+++ src/hooks/__tests__/useCheckbox.test.ts
@@ -1,5 +1,5 @@
 import useQueryCheckbox from '../useCheckbox';
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 
 interface MockCheckboxItemProps {
   key: string;
@@ -18,6 +18,11 @@ function generateCheckboxList(endIndx: number, startIndex = 1) {
   return defaultCheckboxList;
 }
 
+// 转换成key数组
+function toKeyArray(checkboxItemList: MockCheckboxItemProps[]) {
+  return checkboxItemList.map(item => item.key);
+}
+
 describe('hook useQueryCheckbox', () => {
   it('should return the total selected item', () => {
     const checkboxList = generateCheckboxList(8);
@@ -34,4 +39,49 @@ describe('hook useQueryCheckbox', () => {
     const { selected } = result.current;
     expect(selected).toEqual(checkboxList);
   });
+
+  it('should return is all selected result', () => {
+    const checkboxList = generateCheckboxList(8);
+    const { result } = renderHook(() =>
+      useQueryCheckbox(checkboxList, 'key', checkboxList),
+    );
+    const { isAllSelected: isAllSelectedFn } = result.current;
+    const isAllSelected = isAllSelectedFn();
+    expect(isAllSelected).toEqual(true);
+  });
+
+  it('should return is partial selected', () => {
+    const checkboxList = generateCheckboxList(8);
+    const partialCheckboxList = generateCheckboxList(3);
+    const { result } = renderHook(() =>
+      useQueryCheckbox(checkboxList, 'key', partialCheckboxList),
+    );
+    const { isPartialSelected: isPartialSelectedFn } = result.current;
+    const isPartialSelected = isPartialSelectedFn();
+    expect(isPartialSelected).toEqual(true);
+  });
+
+  it('should selected all item and return all checkbox item', () => {
+    const checkboxList = generateCheckboxList(8);
+    const { result } = renderHook(() => useQueryCheckbox(checkboxList, 'key'));
+    act(() => {
+      result.current.selectAll();
+    });
+    expect(toKeyArray(result.current.selected)).toEqual(
+      expect.arrayContaining(toKeyArray(checkboxList)),
+    );
+  });
+
+  it('should unselected all item and return empty result', () => {
+    const checkboxList = generateCheckboxList(8);
+    const { result } = renderHook(() =>
+      useQueryCheckbox(checkboxList, 'key', checkboxList),
+    );
+    act(() => {
+      result.current.unSelectAll();
+    });
+    expect(result.current.selected.length).toEqual(0);
+  });
+
+
 });
